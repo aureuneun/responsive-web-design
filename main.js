84c8acdae184e0807dc07c409d6f86a5777ef9ec
aureuneun <aureuneun@gmail.com>
2020-07-20 16:23:02 +0900

    Responsive Web Design

---
+++ main.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".navbar__menu");
+const icons = document.querySelector(".navbar__icons");
+const toggleBtn = document.querySelector(".navbar__toggleBtn");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  icons.classList.toggle("active");
+});
